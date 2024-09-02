9739151ee2d173bb40c3b2d4f653072ed881eff1
Mohamed Madkour <Mohamed.Madkour2002@gmail.com>
2024-09-02 18:16:32 +0300

    Add project files.

---
+++ MVCDemo.DAL/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCDemo.DAL.Data.Configurations;
+using MVCDemo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDemo.DAL.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = . ;Database = MVCProject ; Trusted_Connection = True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Department>(new DepartmentConfiguration());
+        }
+        public DbSet<Department> Department { get; set; }
+    }
+}
---
+++ MVCDemo.DAL/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDemo.DAL.Models
+{
+    public class Department
+    {
+        public int Id {  get; set; }
+        [Required(ErrorMessage ="Code is Required!")]
+        public string Code { get; set; } // .Net 5.0 Allow Null
+        [Required(ErrorMessage = "Name is Required!")]
+
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
